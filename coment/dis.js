29ff2ec79486ba7a303fe30d03b54ca2ec20803c
RAVIL RAHULKUMAR PATEL <ravil.222012081@rfs.edu.in>
2021-04-21 18:12:11 +0530

    Update dis.js

--- coment/dis.js
+++ coment/dis.js
@@ -1,3 +1,11 @@
 
-
-    document.write('<noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>')
+    var disqus_config = function () {
+    this.page.url = 'https://mock-test.colonelvyas.co.in/tat-1'; 
+    this.page.identifier = '36485'; 
+    };
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://ppdt-mock.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
