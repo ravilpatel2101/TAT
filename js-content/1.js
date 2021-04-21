209c583802c6e9f31883f6d21ed9bff6d2d78259
ravilpatel2101 <ravil.222012081@rfs.edu.in>
2021-04-21 13:21:46 +0530

    Update 1.js

--- js-content/1.js
+++ js-content/1.js
@@ -173,6 +173,6 @@ document.write('<div id=\"disqus_thread\"></div>')
  
 document.write( '  </main>' );
 document.write( '<script> var disqus_config = function () { this.page.url = \'https://mock-test.colonelvyas.co.in/tat-1\'; this.page.identifier = \'36485\'; }; (function() { // DON\'T EDIT BELOW THIS LINE var d = document, s = d.createElement(\'script\'); s.src = \'https://ppdt-mock.disqus.com/embed.js\'; s.setAttribute(\'data-timestamp\', +new Date()); (d.head || d.body).appendChild(s); })(); </script>' );
-document.write('<script id=\"dsq-count-scr\" src=\"//TAT-mock.disqus.com/count.js\" async></script>');
+document.write('<script id=\"dsq-count-scr\" src=\"//ppdt-mock.disqus.com/count.js\" async></script>');
 document.write('<script src=\"basics/basics.js\"></script>');
 
